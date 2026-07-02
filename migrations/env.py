7ba6fff7a1736911916c7b1b8d1ca13ad14a4c09
mcarina <marciacarina931@gmail.com>
2026-07-02 09:33:59 -0400

    arquitutura feature

--- migrations/env.py
+++ migrations/env.py
@@ -8,7 +8,9 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 
 from app.core.database import Base
-from app.models import user, role, user_role, categoria, marca, localizacao, produto, setor, estoque, movimentacao, patrimonio, emprestimo
+from app.models import categoria, marca, localizacao, produto, setor, estoque, movimentacao, patrimonio, emprestimo
+from app.users.models import user, user_role
+from app.roles.models import role
 from app.core.config import settings
 
 # this is the Alembic Config object, which provides
